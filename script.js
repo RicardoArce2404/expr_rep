3f7d9db32b284d498a39088a7bd2a0e26f382cac
RicardoArce2404 <yasserarce2404@gmail.com>
2023-07-28 22:35:26 -0600

    Funciones básicas

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function replaceChars() {
+
+  const chars_replace = [
+    ['equivale a', '≡'],
+    ['implica taut', '⇒'],
+    ['solo si taut', '⇔'],
+    ['implica', '→'],
+    ['solo si', '↔'],
+    ['no', '¬'],
+    ['y', '∧'],
+    ['o', '∨']
+  ]
+
+  let text = document.getElementById('textInput').value
+  let textOutput = document.getElementById('textOutput')
+
+  for (var i = 0; i < chars_replace.length; i++) {
+    text = text.replaceAll(chars_replace[i][0], chars_replace[i][1]);
+  }
+
+  textOutput.value = text;
+  console.log(text)
+
+}
+
+async function copyText() {
+  let text = document.getElementById('textOutput').value;
+  try { await navigator.clipboard.writeText(text); }
+  catch (err) { alert('Error al copiar: ', err); }
+  finally { alert('Texto copiado ✔️') }
+}
